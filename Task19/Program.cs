08b7d087853af193826a421347cdae963469cc88
Ekaterina <ekaterina_r21@mail.ru>
2022-04-24 13:32:48 +0300

    Задача с палиндромом

---
+++ Task19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Является ли пятизначное число палиндромом
+Console.WriteLine("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+int num1 = num/10000;
+int num2 = num%10;
+int num3 = num/1000%10;
+int num4 = num%100/10;
+if(num1 == num2 & num3 == num4) 
+{
+    Console.WriteLine($"Число {num} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {num} не является палиндромом");
+}
