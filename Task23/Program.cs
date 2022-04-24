09ab0a6e8745c0d39d0816c5b0013aebd2574583
Ekaterina <ekaterina_r21@mail.ru>
2022-04-24 14:00:55 +0300

    Возведение в куб

---
+++ Task23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Таблица кубов чисел от 1 до N
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int index = 1;
+while (index <= N)
+{
+    Console.WriteLine(Math.Pow(index,3));
+    index++;
+}
